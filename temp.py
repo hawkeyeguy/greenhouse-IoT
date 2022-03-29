9b22c6ed6c07dbac2eadf194afa5ae68594a5ed7
hawkeyeguy <66351727+hawkeyeguy@users.noreply.github.com>
2022-03-29 21:03:37 +0100

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+import glob
+import time
+import datetime
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+bufsize=0
+f=open('/var/log/temp.log','w',bufsize)
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+	
+while True:
+	temp=read_temp()
+	print(temp)	
+	now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	f.write(str(now)+str(temp)+"\n")
+	time.sleep(900)
+
